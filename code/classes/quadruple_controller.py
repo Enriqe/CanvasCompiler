76129dc10d00002519b16a8166a9da109ed5d583
Enrique Marroquin <enrique.marr@gmail.com>
2017-04-03 10:52:08 -0500

    forgot to add quad to list somewhere

--- code/classes/quadruple_controller.py
+++ code/classes/quadruple_controller.py
@@ -1,26 +1,5 @@
 from quadruple import Quadruple
 
-# priority = {
-#         '=' : 0,
-#         'and' : 1,
-#         'or' : 1,
-#         '>' : 2,
-#         '<' : 2,
-#         '>=' : 2,
-#         '<=' : 2,
-#         '!=' : 2,
-#         '==' : 2,
-#         '+' : 3,
-#         '-' : 3,
-#         '*' : 4,
-#         '/' : 4,
-#         '(' : 5,
-#         ')' : 5
-#         }
-
-# def has_higher_priority(op1, op2):
-#     return priority[op1] > priority[op2]
-
 class QuadrupleController:
     quad_list = []
     operator_stack = []
@@ -45,7 +24,9 @@ class QuadrupleController:
             quad = Quadruple("=", left_opnd, right_opnd)
             res = quad.generate_quad()
 
-            self.quad_list.append(res)
+            self.quad_list.append(quad)
+            for q in self.quad_list:
+                print("quad: ", q.operator, q.left_operand, q.right_operand)
             print("ASGN RES", res)
         else:
             self.operator_stack.append("=")
@@ -76,82 +57,3 @@ class QuadrupleController:
 
             self.operand_stack.append(result)
             self.quad_list.append(quad)
-
-
-
-
-
-    # '''
-    # read_operator:
-    # Reads current operator in expression and performs corresponding operation
-    # @param current_op: char of operator that is currently being read
-    # '''
-    # def read_operator(self, current_op):
-    #     # checks if operator_stack is empty
-    #     if len(self.operator_stack) == 0:
-    #         self.operator_stack.append(current_op)
-    #     else:
-    #         prev_op = self.operator_stack[-1]
-
-    #         #TODO: create a priority list/dict/enum/w.e. to compare priorities between ops
-    #         if(has_higher_priority(prev_op, current_op)):
-    #             # loop through operator_stack while a higher-priority operator than the current_op
-    #             # is found
-    #             while(has_higher_priority(prev_op, current_op) and len(self.operator_stack) > 0):
-    #                 #TODO: should we check for a uniary operator?
-
-    #                 # Pops from stacks and generates Quad
-    #                 temp_op = self.operator_stack.pop()
-    #                 right_opnd = self.operand_stack.pop()
-    #                 left_opnd = self.operand_stack.pop()
-
-    #                 #TODO: create generate_quad() method
-    #                 temp_quad = Quadruple(left_opnd, right_opnd, temp_op)
-    #                 result = temp_quad.generate_quad()
-
-    #                 #TODO: check if result is valid
-    #                 if result:
-    #                     self.operand_stack.append(result)
-    #                     self.quad_list.append(temp_quad)
-    #                 else:
-    #                     print "ERROR, operation not valid"
-
-    #                 prev_op = self.operator_stack[-1]
-
-    #         self.operator_stack.append(current_op)
-
-
-    # '''
-    # finished_expression:
-    # To be ran when expression is finished. Empties both operator_stack and operand_stack
-    # and generates corresponding quadruples.
-
-
-    # '''
-    # def finished_expression(self):
-    #     #TODO: check for equals sign
-    #     prev_op = self.operator_stack[-1]
-    #     while( len(self.operator_stack) > 0 and prev_op != '='):
-    #         # Pops from stacks and generates Quad
-    #         temp_op = self.operator_stack.pop()
-    #         right_opnd = self.operand_stack.pop()
-    #         left_opnd = self.operand_stack.pop()
-
-    #         #TODO: create generate_quad() method
-    #         temp_quad = Quadruple(left_opnd, right_opnd, temp_op)
-    #         result = temp_quad.generate_quad()
-
-    #         #TODO: check if result is valid
-    #         if result:
-    #             self.operand_stack.append(result)
-    #             self.quad_list.append(temp_quad)
-    #         else:
-    #             #TODO: call error exit
-    #             print "ERROR, operation not valid"
-
-    #         prev_op = self.operator_stack[-1]
-
-    #     if(prev_op == '='):
-    #         operand = self.operand_stack.pop()
-    #         op = self.operator
-    #         temp_quad = Quadruple(left_opnd, "", operand)
