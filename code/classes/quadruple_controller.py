ab92988971ed95a7ad4ca1c9bf36e9fda7cae140
Enrique Marroquin <enrique.marr@gmail.com>
2017-05-03 10:29:05 -0500

    documenting..

--- code/classes/quadruple_controller.py
+++ code/classes/quadruple_controller.py
@@ -15,7 +15,11 @@ def debug(right, right_type, left, left_type, op):
         print(left)
         print(left_type)
         print(op)
-
+'''
+QuadrupleController manages quadruple creation, quaduple filling, jump stack, 
+operator stack, type stack, and operand stack. It keeps count of the quads that 
+are being added to the quad list
+'''
 class QuadrupleController:
     quad_list = []
     operator_stack = []
@@ -27,30 +31,38 @@ class QuadrupleController:
     quad_counter = 0
     memory_controller = MemoryController()
 
+    # Adds quadruple to list of quadruples
     def add_quadruple(self, quad):
         self.quad_list.append(quad)
         self.quad_counter = self.quad_counter + 1
 
+    # Reads an operator while pushing it to the operator stack
     def read_operator(self, current_op):
         self.operator_stack.append(current_op)
 
+    #Reads an operand and pushes it to the operand stack
     def read_operand(self, current_opnd):
         self.operand_stack.append(current_opnd)
 
+    # Reads at the same time as an operand, pushes its type to the type stack
     def read_type(self, type):
         self.type_stack.append(type)
 
+    # Pushes a fake bottom to the operator stack. Used for list index and func calling args
     def read_fake_bottom(self):
         self.operator_stack.append(self.fake_bottom)
 
+    # Pops fake bottom from stack
     def pop_fake_bottom(self):
         self.operator_stack.pop()
 
+    # Fills a quad given its location in the list, with its quad_num
     def fill(self, loc, quad_num):
         quad = self.quad_list[loc]
         quad.add_location(quad_num)
         self.quad_list[loc] = quad
 
+    # runs after an expression is finished, performs corresponding operation depending on the operator and operands in the stacks
     def finished_expression(self, lineno):
         if(len(self.operator_stack) > 0 and self.operator_stack[-1] != '('):
             right_opnd = self.operand_stack.pop()
@@ -66,15 +78,18 @@ class QuadrupleController:
                 print("ERROR: type mismatch, line " + str(lineno))
                 exit()
 
+    # pushes endproc quad to quad list
     def finished_function(self):
         #quad = self.quad_list[-1] # peek last quad before return
         quad = Quadruple("ENDPROC")
         self.add_quadruple(quad)
 
+    # pushes era quad to quad list
     def function_call_init(self, virt_address):
         quad = Quadruple("ERA", virt_address)
         self.add_quadruple(quad)
 
+    # pushes param list to quads
     def function_call_param(self, param_address):
         val_address = self.operand_stack.pop()
         val_type = self.type_stack.pop()
@@ -83,10 +98,12 @@ class QuadrupleController:
             quad = Quadruple("PARAM", val_address, '', param_address)
             self.add_quadruple(quad)
 
+    # generates gosub quadruple
     def function_gosub(self, virt_address, jump_to_function_index):
         quad = Quadruple("GOSUB", virt_address, '', jump_to_function_index)
         self.add_quadruple(quad)
 
+    # generates quads belonging to array accessing
     def array_access(self, index, arr_size, base_address, next_temp):
         quad = Quadruple("VER", "", index, arr_size)
         self.add_quadruple(quad)
@@ -95,35 +112,42 @@ class QuadrupleController:
         self.type_stack.pop()
         self.add_quadruple(quad)
 
+    # pops from stacks unwanted base address of array
     def after_array_check(self):
         self.type_stack.pop()
         return self.operand_stack.pop()
 
 ################### Canvas Custom Operations ###################
 
+    # prints a statment
     def print_stmt(self):
         addr = self.operand_stack.pop()
         self.type_stack.pop()
         quad = Quadruple("PRINT", "", "", addr)
         self.add_quadruple(quad)
 
+    # generates main quad for later filling
     def program_start(self):
         quad = Quadruple("MAIN")
         self.add_quadruple(quad)
         self.jump_stack.append(self.quad_counter - 1)
 
+    # main quad is now filled with index of quad where main function starts
     def main_start(self):
         main_quad_index = self.jump_stack.pop()
         self.fill(main_quad_index, self.quad_counter)
     
+    # generates quad for canvas
     def create_canvas(self):
         quad = Quadruple("CANVAS")
         self.add_quadruple(quad)
 
+    # genertes quad for paint
     def paint_canvas(self):
         quad = Quadruple("PAINT")
         self.add_quadruple(quad)
 
+    # generates quad for shapes
     def create_shape(self, addr, x, y, shape_type):
         if shape_type == "circle":
             quad = Quadruple("CIRCLE", addr, x, y)
@@ -133,10 +157,11 @@ class QuadrupleController:
 
 ################### Conditionals ###################
 
-    def finished_conditional(self):
+    def finished_conditional(self): #f finished condition
         end = self.jump_stack.pop()
         self.fill(end, self.quad_counter)
 
+    # after condition expression quad
     def after_cond_expression(self):
         res = self.operand_stack.pop()
         self.type_stack.pop()
@@ -144,12 +169,14 @@ class QuadrupleController:
         self.add_quadruple(quad)
         self.jump_stack.append(self.quad_counter - 1)
 
+    #after else if condition expression quad
     def after_elsif_expression(self):
         wait = self.jump_stack.pop()
         jump = self.jump_stack.pop()
         self.fill(jump, self.quad_counter)
         self.jump_stack.append(wait)
 
+    # after else condition expression quad
     def after_else(self):
         quad = Quadruple("GOTO")
         self.add_quadruple(quad)
