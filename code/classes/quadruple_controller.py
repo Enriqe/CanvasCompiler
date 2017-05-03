5b47e7c4c3f414fd1079fcc1d8a347733b2089d8
Enrique Marroquin <enrique.marr@gmail.com>
2017-05-03 01:22:21 -0500

    adds exp to arrays

--- code/classes/quadruple_controller.py
+++ code/classes/quadruple_controller.py
@@ -53,9 +53,6 @@ class QuadrupleController:
         self.quad_list[loc] = quad
 
     def finished_expression(self, lineno):
-        print "STACKS"
-        print self.operator_stack
-        print self.operand_stack
         if(len(self.operator_stack) > 0 and self.operator_stack[-1] != '('):
             right_opnd = self.operand_stack.pop()
             left_opnd = self.operand_stack.pop()
@@ -99,10 +96,13 @@ class QuadrupleController:
         quad = Quadruple("VER", "", index, arr_size)
         self.add_quadruple(quad)
         quad = Quadruple("ADDBASE", index, base_address, next_temp)
+        self.operand_stack.pop()
+        self.type_stack.pop()
         self.add_quadruple(quad)
 
     def after_array_check(self):
-        self.operand_stack.pop()
+        self.type_stack.pop()
+        return self.operand_stack.pop()
 
 ################### Canvas Custom Operations ###################
 
